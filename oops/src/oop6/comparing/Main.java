2877086ec8d13d8357b0fbaacb20fcbde930fe6c
Guruprakash-coder <guruprakash6999@gmail.com>
2026-01-28 12:16:33 +0530

    update

--- oops/src/oop6/comparing/Main.java
+++ oops/src/oop6/comparing/Main.java
@@ -9,7 +9,7 @@ public class Main {
         Student gopal=new Student(7,72.77f);
         Student mani=new Student(28,95.77f);
         Student jeeva=new Student(43,89.77f);
-
+        
         Student[] list={gurup,rahul,gopal,mani,jeeva};
         System.out.println(Arrays.toString(list));
         Arrays.sort(list);
