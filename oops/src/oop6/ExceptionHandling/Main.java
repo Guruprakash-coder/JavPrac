f154b4a363546476c586e9a1daf3cc32ea126e40
Guruprakash-coder <guruprakash6999@gmail.com>
2026-01-28 17:03:33 +0530

    update

--- oops/src/oop6/ExceptionHandling/Main.java
+++ oops/src/oop6/ExceptionHandling/Main.java
@@ -3,21 +3,24 @@ package oop6.ExceptionHandling;
 public class Main {
     public static void main(String[] args) {
         int a = 5;
-        int b = 0;
+        int b = 2;
         try {
-            divide(a,b);
-        } catch (Exception e) {
+            System.out.println(divide(a,0));
+            throw new Exception("just for fun");
+        } catch (ArithmeticException e) {
             System.out.println(e.getMessage());
-        } finally {
+        } catch (Exception e) {
+            System.out.println("normal exception");
+        }finally {
             System.out.println("Vanakam mame");
         }
 
 
     }
-    static int divide(int a, int b)
+     public static int divide(int a, int b) throws Exception
     {
         if(b==0){
-            throw new ArithmeticException("pls do not ddivide by zero");
+            throw new Exception("pls do not ddivide by zero");
         }
         return a/b;
     }
---
+++ oops/src/oop6/ExceptionHandling/demo.java
@@ -0,0 +1,7 @@
+package oop6.ExceptionHandling;
+
+public class demo {
+    public static void main(String[] args) throws Exception {
+        System.out.println(Main.divide(3,0));
+    }
+}
