5a39bdb0b6207401a05c59381c37b4b130377c46
Guruprakash-coder <guruprakash6999@gmail.com>
2026-02-17 22:00:52 +0530

    update

---
+++ oops/src/oop7/collections/basic.java
@@ -0,0 +1,12 @@
+package oop7.collections;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class basic {
+    List<Integer> list=new ArrayList<>();
+    List<Integer> list2=new LinkedList<>();
+
+}
