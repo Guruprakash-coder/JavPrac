51e5cb991726e0989f5f86ff3cc5d5fdb788e59e
Guruprakash-coder <guruprakash6999@gmail.com>
2026-02-18 22:48:22 +0530

    update

--- oops/src/oop7/enumex/Basic.java
+++ oops/src/oop7/enumex/Basic.java
@@ -1,19 +1,30 @@
 package oop7.enumex;
 
 public class Basic {
-    enum Week{
-        Monday,tuesday,wednesday,thursday,friday,saturday,sunday
+    enum Week implements A{
+        Monday,tuesday,wednesday,thursday,friday,saturday,sunday;
         //enumeration
         //public static and final
         // type is week
+
+        Week(){
+            System.out.println("Constructor called for "+this);
+        }
+
+        @Override
+        public void hello()  {
+            System.out.println("Hello how are you");
+        }
     }
 
     public static void main(String[] args) {
         Week week;
         week=Week.friday;
 
+
         for(Week day: Week.values()){
             System.out.println(day);
         }
+        System.out.println(week.ordinal());
     }
 }
