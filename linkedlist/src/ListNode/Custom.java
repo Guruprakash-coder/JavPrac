584a3f2edfb38a4d59ee420846cf293dd8582724
Guruprakash-coder <guruprakash6999@gmail.com>
2026-03-05 12:08:57 +0530

    update

--- linkedlist/src/ListNode/Custom.java
+++ linkedlist/src/ListNode/Custom.java
@@ -32,6 +32,54 @@ public class Custom {
         head=prev;
         return head;
     }
+    public boolean isPalindrome(ListNode head) {
+
+        ListNode mid=middleNode(head);
+        ListNode headsecond=reverseList(mid);
+
+        while(head!=null && headsecond!=null){
+            if(head.val!=headsecond.val){
+                return false;
+            }
+            head=head.next;
+            headsecond=headsecond.next;
+        }
+
+        return true;
+
+    }
+//    public ListNode reverseList(ListNode node){
+//        ListNode prev=null;
+//        ListNode pres=node;
+//        ListNode Next=pres.next;
+//
+//        while(pres!=null){
+//            pres.next=prev;
+//            prev=pres;
+//            pres=Next;
+//            if(Next!=null){
+//                Next=Next.next;
+//            }
+//
+//        }
+//        node=prev;
+//        return node;
+//
+//
+//    }
+    public ListNode middleNode(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!= null){
+            ListNode temp=slow.next;
+            slow=temp;
+            temp=fast.next.next;
+            fast=temp;
+
+        }
+
+        return slow;
+    }
     public ListNode reverseList(ListNode head,int left,int right){
         if(left==right){
             return head;
