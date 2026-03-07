af78a7c4e6a609d69cb5d326347078b768c871aa
Guruprakash-coder <guruprakash6999@gmail.com>
2026-03-07 16:13:24 +0530

    update

--- linkedlist/src/ListNode/Custom.java
+++ linkedlist/src/ListNode/Custom.java
@@ -56,10 +56,56 @@ public class Custom {
             prev=newEnd;
         }
         return head;
+    }
+
+    public ListNode reverseKGroupalt(ListNode head, int k) {
+        if(k<=1 || head==null){
+            return head;
+        }
+        ListNode prev=null;
+        ListNode pres=head;
+        while(true){
+            ListNode temp = pres;
+            int count = 0;
+
+            while (temp != null && count < k) {
+                temp = temp.next;
+                count++;
+            }
+
+            if (count < k) {
+                if (prev != null) prev.next = pres;
+                break;
+            }
 
 
+            ListNode last=prev;
+            ListNode newEnd=pres;
 
+            ListNode Next=pres.next;
 
+            for(int i=0;pres!=null && i<k;i++){
+                pres.next=prev;
+                prev=pres;
+                pres=Next;
+                if(Next!=null){
+                    Next=Next.next;
+                }
+            }
+
+            if(last!=null){
+                last.next=prev;
+            }else{
+                head=prev;
+            }
+
+            newEnd.next=pres;
+            if(pres==null){
+                break;
+            }
+            prev=newEnd;
+        }
+        return head;
     }
     public ListNode reverseList(ListNode head) {
         if(head==null || head.next==null){
