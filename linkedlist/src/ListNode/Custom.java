7aceeccb062e6dd94b8c6250ce85067cbb33dba6
Guruprakash-coder <guruprakash6999@gmail.com>
2026-03-08 00:00:12 +0530

    update

--- linkedlist/src/ListNode/Custom.java
+++ linkedlist/src/ListNode/Custom.java
@@ -110,6 +110,38 @@ public class Custom {
         }
         return head;
     }
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head == null || head.next == null || k == 0){
+            return head;
+        }
+        int length=0;
+        ListNode temp=head;
+        while(temp!=null){
+            length++;
+            temp=temp.next;
+        }
+        k=k%length;
+        if(k==0){
+            return head;
+        }
+        temp=head;
+        for(int i=0;i<length-k-1;i++){
+            temp=temp.next;
+        }
+
+        ListNode first=temp.next;
+        temp.next=null;
+        ListNode last=first;
+        System.out.print(last.val);
+        while(last.next!=null){
+
+            last=last.next;
+        }
+
+        last.next=head;
+        head=first;
+        return head;
+    }
     public ListNode reverseList(ListNode head) {
         if(head==null || head.next==null){
             return head;
