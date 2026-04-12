6ff4f28f43cb59d282cf84f7a328907aed6a5a39
Guruprakash-coder <guruprakash6999@gmail.com>
2026-04-12 15:10:28 +0530

    update

--- stack and queue/src/Queue/cusQueue.java
+++ stack and queue/src/Queue/cusQueue.java
@@ -3,7 +3,7 @@ package Queue;
 public class cusQueue {
     private int[] data;
     private static final int DEFAULT_SIZE=10;
-    int end=-1;
+    int end=0;
 
     public cusQueue(){
         this(DEFAULT_SIZE);
@@ -12,9 +12,27 @@ public class cusQueue {
         this.data=new int[n];
      }
     public boolean isFull(){
-        return end==data.length-1;
+        return end==data.length;
     }
     public boolean isEmpty(){
-        return end==-1;
+        return end==0;
+    }
+    public boolean insert(int item){
+        if(isFull()){
+            return false;
+        }
+        data[end++]=item;
+        return true;
+    }
+    public int remove() throws Exception {
+        if(isEmpty()){
+            throw new Exception("no item to remove in queue");
+        }
+        int remove=data[0];
+        for(int i=1;i<end;i++){
+            data[i-1]=data[i];
+        }
+        end--;
+        return remove;
     }
 }
