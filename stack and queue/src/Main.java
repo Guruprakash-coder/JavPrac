d82d7c8b54cda8b4c1ad0c1f0ce0dd4f6c762d4e
Guruprakash-coder <guruprakash6999@gmail.com>
2026-04-12 12:37:56 +0530

    update

--- stack and queue/src/Main.java
+++ stack and queue/src/Main.java
@@ -1,22 +1,23 @@
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Stack;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
-        StringBuilder sb=new StringBuilder();
-        sb.append("sd");
-        List<Integer> s=new ArrayList<>();
-        
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Stack<Integer> s=new Stack<>();
+        s.push(32);
+        s.push(92);
+        s.push(22);
+        s.push(2);
+        s.push(321);
+        System.out.println(s.pop());
+        System.out.println(s.pop());
+        System.out.println(s.pop());
+        System.out.println(s.pop());
+        System.out.println(s.pop());
     }
 }
