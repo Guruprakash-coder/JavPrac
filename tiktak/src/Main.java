632da85df98f3b059097f843bbb304900e6659b6
Guruprakash-coder <guruprakash6999@gmail.com>
2026-04-20 00:16:05 +0530

    update

--- tiktak/src/Main.java
+++ tiktak/src/Main.java
@@ -48,7 +48,7 @@ public class Main {
         if(board[0][0]==player && board[1][1]==player && board[2][2]==player){
             return true;
         }
-        if(board[0][2]==player && board[1][1]==player && board[2][1]==player){
+        if(board[0][2]==player && board[1][1]==player && board[2][0]==player){
             return true;
         }
         return false;
