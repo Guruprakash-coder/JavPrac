d5234796e76c423b0e507be1ee401ab28bc71237
Guruprakash-coder <guruprakash6999@gmail.com>
2026-05-26 23:26:01 +0530

    update

--- Trees/src/implement/BST.java
+++ Trees/src/implement/BST.java
@@ -41,6 +41,8 @@ public class BST {
         if(value>node.value){
             node.right=insert(value, node.right);
         }
+        node.height=Math.max(height(node.left),height(node.right))+1;
+        return node;
     }
     public void display(){
         display(root,"Root Node:");
