cc128a924e29c5c25350319e575752d34da55e46
Guruprakash-coder <guruprakash6999@gmail.com>
2026-05-29 00:05:03 +0530

    update

--- Trees/src/implement/Main.java
+++ Trees/src/implement/Main.java
@@ -2,7 +2,7 @@ package implement;
 
 public class Main {
     public static void main(String[] args) {
-        AVL tree=new AVL();
+        BST tree=new BST();
 
         int[] arr= new int[]{10, 20, 30, 40, 50, 60, 78, 92, 112, 440};
         tree.populatedSorted(arr);
