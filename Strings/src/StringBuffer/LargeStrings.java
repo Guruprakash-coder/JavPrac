e34e114e51f1182435203184199f1bfb383ed58d
Guruprakash-coder <guruprakash6999@gmail.com>
2026-05-29 23:20:27 +0530

    update

--- Strings/src/StringBuffer/LargeStrings.java
+++ Strings/src/StringBuffer/LargeStrings.java
@@ -5,6 +5,9 @@ public class LargeStrings {
         StringBuffer buffer=new StringBuffer();
         buffer.append("Guruprakash");
         String str= buffer.toString();
+        buffer.append(4);
+        buffer.insert(2,"rahul");
+        buffer.replace(0,buffer.length(),"hello");
         System.out.println(buffer);
 
     }
