14e7bd6acf8e354898b71ed4cf6bede05aecc635
Guruprakash-coder <guruprakash6999@gmail.com>
2026-06-05 12:45:28 +0530

    update

--- File handling/src/Main.java
+++ File handling/src/Main.java
@@ -1,9 +1,23 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.stream.Stream;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main{
     public static void main(String[] args) {
-
+        try(InputStreamReader isr=new InputStreamReader(System.in)){
+            System.out.println("Enter some letters:");
+            int letters=isr.read();
+            while(isr.ready()){
+                System.out.println((char)letters);
+                letters= isr.read();
+            }
+            isr.close();
+            System.out.println();
+        }catch(IOException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
