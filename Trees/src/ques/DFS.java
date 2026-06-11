ba1fd794dfad2951e41a2144020a46d5276c870b
Guruprakash-coder <guruprakash6999@gmail.com>
2026-06-12 00:37:16 +0530

    update

--- Trees/src/ques/DFS.java
+++ Trees/src/ques/DFS.java
@@ -1,5 +1,9 @@
 package ques;
 
+import implement.BST;
+
+import java.util.Arrays;
+
 public class DFS {
 
 
@@ -112,6 +116,21 @@ public class DFS {
     }
     //very very important int question
     public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if(preorder.length==0){
+            return null;
+        }
+        int r=preorder[0];
+        int index=0;
 
+        for(int i=0;i<inorder.length;i++){
+            if(inorder[i]==r){
+                index=i;
+                break;
+            }
+        }
+        TreeNode node=new TreeNode(r);
+        node.left=buildTree(Arrays.copyOfRange(preorder,1,index+1),Arrays.copyOfRange(inorder,0,index));
+        node.right=buildTree(Arrays.copyOfRange(preorder,index+1,preorder.length),Arrays.copyOfRange(inorder,index+1,inorder.length));
+        return node;
     }
 }
