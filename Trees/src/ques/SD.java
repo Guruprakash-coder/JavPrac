be2b38557a3cc613cdab19218dddedf76e2ef5fc
Guruprakash-coder <guruprakash6999@gmail.com>
2026-06-12 14:42:40 +0530

    update

---
+++ Trees/src/ques/SD.java
@@ -0,0 +1,13 @@
+package ques;
+
+public class SD {
+    private static class TreeNode {
+        private int value;
+        TreeNode left;
+        TreeNode right;
+        public TreeNode(int value){
+            this.value=value;
+        }
+    }
+    private TreeNode root;
+}
