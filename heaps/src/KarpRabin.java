bb45a5ad1ad7c4b8209a7cf6a902ffee5ecca3cb
Guruprakash-coder <guruprakash6999@gmail.com>
2026-06-16 00:58:11 +0530

    update

--- heaps/src/KarpRabin.java
+++ heaps/src/KarpRabin.java
@@ -15,4 +15,20 @@ public class KarpRabin {
         newHash=(long)(newHash+newChar*Math.pow(PRIME,patterLength-1));
         return newHash;
     }
+    public void search(String text,String pattern){
+        int patternLength=pattern.length();
+        long patternHash=calculateHash(pattern);
+        long textHash=calculateHash(text.substring(0,patternLength));
+
+        for(int i=0;i<=text.length()-patternLength;i++){
+            if(textHash == patternHash) {
+                if(text.substring(i, i+patternLength).equals(pattern)) {
+                    System.out.println("Pattern found at index " + i);
+                }
+            }
+            if (i < text.length() - patternLength) {
+                textHash = updateHash(textHash, text.charAt(i),text.charAt(i+patternLength),patternLength);
+            }
+        }
+    }
 }
