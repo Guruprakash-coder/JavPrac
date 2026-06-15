0e8e02485183f1e2bc03bbdbab36be6375709115
Guruprakash-coder <guruprakash6999@gmail.com>
2026-06-15 15:46:35 +0530

    update

--- heaps/src/Main.java
+++ heaps/src/Main.java
@@ -1,9 +1,13 @@
-import java.util.PriorityQueue;
+import java.util.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        
+        HashMap<String, Integer> map=new HashMap<>();
+        map.put("gp",80);
+        map.put("ehdvjwb",832);
+        map.put("gjwfbedp",8420);
+        System.out.println(map.get("gp"));
     }
 }
