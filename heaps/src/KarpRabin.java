0c400429338b70a78029d204184136e0606e59a0
Guruprakash-coder <guruprakash6999@gmail.com>
2026-06-16 00:44:13 +0530

    update

--- heaps/src/KarpRabin.java
+++ heaps/src/KarpRabin.java
@@ -3,8 +3,15 @@ public class KarpRabin {
 
     private long calculateHash(String str){
         long hash=0;
+        for(int i=0;i<str.length();i++){
+            hash=(long)(hash+str.charAt(i)*Math.pow(PRIME,i));
 
+        }
 
         return hash;
+    }
+    private long updateHash(long prevHash,char oldChar,char newChar,int patterLength){
+        long newHash=(prevHash-oldChar)/PRIME;
+
     }
 }
