cdf78d8a20b4c8dba8b4445bcf439b0812cf924d
Guruprakash-coder <guruprakash6999@gmail.com>
2026-06-16 15:49:27 +0530

    update

--- Arrays/src/countSort.java
+++ Arrays/src/countSort.java
@@ -4,6 +4,23 @@ public class countSort {
             return;
         }
         int max=arr[0];
+        for(int num:arr){
+            if(num>max){
+                max=num;
+            }
+        }
+        int[] countArray=new int[max+1];
+        for(int num:arr){
+            countArray[num]++;
+        }
+        int index=0;
+        for(int i=0;i<max;i++){
+            while(countArray[i]>0){
+                arr[index]=i;
+                index++;
+                countArray[i]--;
+            }
+        }
 
     }
 }
