e4b8a2615d21ad5f0009048027459dc484e542e8
Guruprakash-coder <guruprakash6999@gmail.com>
2026-06-17 14:46:00 +0530

    update

--- Arrays/src/RadixSort.java
+++ Arrays/src/RadixSort.java
@@ -5,8 +5,11 @@ public class RadixSort {
         int max= Arrays.stream(arr).max().getAsInt();
 
 
-        for(int exp=1;;exp=exp*10){
-
+        for(int exp=1;max/exp>0;exp=exp*10){
+            countSort(arr,exp);
         }
+    }
+    private static void countSort(int[] arr,int exp){
+
     }
 }
