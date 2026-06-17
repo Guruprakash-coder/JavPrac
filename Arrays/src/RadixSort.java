88657d95abc42a410c4ad671eec8d5889a101fb5
Guruprakash-coder <guruprakash6999@gmail.com>
2026-06-17 14:43:32 +0530

    update

---
+++ Arrays/src/RadixSort.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class RadixSort {
+    public static void radic(int[] arr){
+        int max= Arrays.stream(arr).max().getAsInt();
+
+
+        for(int exp=1;;exp=exp*10){
+
+        }
+    }
+}
