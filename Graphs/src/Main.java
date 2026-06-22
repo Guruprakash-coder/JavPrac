a99e1c4592ce4ef489f3f966d6296c410cf5bb72
Guruprakash-coder <guruprakash6999@gmail.com>
2026-06-23 00:49:26 +0530

    update

--- Graphs/src/Main.java
+++ Graphs/src/Main.java
@@ -6,7 +6,7 @@ import java.util.Queue;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    ArrayList<Integer> bfsOfGraph(int v,ArrayList<ArrayList<Integer>> adj){
+    List<Integer> bfsOfGraph(int v,ArrayList<ArrayList<Integer>> adj){
         boolean[] vis=new boolean[v];
         List<Integer> bfs=new ArrayList<>();
         vis[0]=true;
@@ -20,12 +20,29 @@ public class Main {
                     q.offer(n);
                     vis[n]=true;
                 }
-
             }
 
         }
         return bfs;
 
+    }
+    public ArrayList<Integer> dfsOfGraph(ArrayList<ArrayList<Integer>> adj){
+        boolean[] vis=new boolean[adj.size()];
+        vis[0]=true;
+        ArrayList<Integer> ans=new ArrayList<>();
+        dfs(0,vis,adj,ans);
+        return ans;
+    }
+    private static void dfs(int node,boolean[] vis,ArrayList<ArrayList<Integer>> adj,ArrayList<Integer> ls){
+        vis[node]=true;
+        ls.add(node);
+        for(int i=0;i<adj.get(node).size();i++){
+            int neigh=adj.get(node).get(i);
+            if(!vis[neigh]){
+                dfs(neigh,vis,adj,ls);
+            }
+
+        }
     }
 
 }
