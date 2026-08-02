5aea5a7b9e68d41d7556e71d8a75a5d083df59d5
Guruprakash-coder <guruprakash6999@gmail.com>
2026-08-03 00:48:20 +0530

    update

--- Graphs/src/alienDictionary.java
+++ Graphs/src/alienDictionary.java
@@ -1,7 +1,35 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class alienDictionary {
+    List<Integer> toposort(int k,List<List<Integer>> adj){
+        int[] inDegree=new int[k];
+        for(int i=0;i<adj.size();i++){
+            for(int j:adj.get(i)){
+                inDegree[j]++;
+            }
+        }
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<inDegree.length;i++){
+            if(inDegree[i]==0){
+                q.add(i);
+            }
+        }
+        List<Integer> topo=new ArrayList<>();
+        while(!q.isEmpty()){
+            int node=q.poll();
+            topo.add(node);
+            for(int neigh:adj.get(node)){
+                if(--inDegree[neigh]==0){
+                    q.offer(neigh);
+                }
+            }
+        }
+        return topo;
+
+    }
     String findOrder(String[] dict,int N,int k){
         List<List<Integer>> adj=new ArrayList<>();
         for(int i=0;i<k;i++){
@@ -17,6 +45,13 @@ public class alienDictionary {
                     break;
                 }
             }
+
         }
+        List<Integer> topo=toposort(k,adj);
+        StringBuilder ans=new StringBuilder();
+        for(int i:topo){
+            ans.append(i+'a');
+        }
+        return ans.toString();
     }
 }
