f26e7dd00240180386ef9ea9bae081c3f6ad1f4f
Guruprakash-coder <guruprakash6999@gmail.com>
2026-08-06 22:21:55 +0530

    update

---
+++ Graphs/src/unDirected.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class unDirected {
+    public int[] shortestPath(int[][] edges,int n,int m,int src){
+        List<List<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int i=0;i<m;i++){
+            adj.get(edges[i][0]).add(edges[i][1]);
+            adj.get(edges[i][1]).add(edges[i][0]);
+        }
+        int dist[]=new int[n];
+        Arrays.fill(dist,10_000);
+        Queue<Integer> q=new LinkedList<>();
+
+    }
+
+}
