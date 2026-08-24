9da861f36beffb26d1550d6022303fcae7402577
Guruprakash-coder <guruprakash6999@gmail.com>
2026-08-24 22:23:29 +0530

    update

--- dp/src/ninjasTraining.java
+++ dp/src/ninjasTraining.java
@@ -1,11 +1,30 @@
+import java.util.Arrays;
+
 public class ninjasTraining {
     int ninjaTraining(int n,int[][] points){
         int dp[][]=new int[n][3];
+        Arrays.fill(dp,-1);
         return f(n-1,3,points,dp);
     }
     int f(int day,int last,int[][] points,int[][] dp){
         if(day==0){
-
+            int maxi=0;
+            for(int i=0;i<3;i++){
+                if(i==last) continue;
+                maxi=Math.max(maxi,points[0][i]);
+            }
+            return maxi;
         }
+        if(dp[day][last]!=-1) return dp[day][last];
+        int maxi=0;
+        for(int i=0;i<3;i++){
+            if(i!=last){
+                int point=points[day][i]+f(day-1,i,points,dp);
+                maxi=Math.max(maxi,point);
+            }
+        }
+
+
+        return dp[day][last];
     }
 }
