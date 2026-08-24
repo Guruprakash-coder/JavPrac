03ba0d68cf5383dd9128d1227291225ec1fd5bd1
Guruprakash-coder <guruprakash6999@gmail.com>
2026-08-24 22:11:26 +0530

    update

--- dp/src/ninjasTraining.java
+++ dp/src/ninjasTraining.java
@@ -1,3 +1,11 @@
 public class ninjasTraining {
+    int ninjaTraining(int n,int[][] points){
+        int dp[][]=new int[n][3];
+        return f(n-1,3,points,dp);
+    }
+    int f(int day,int last,int[][] points,int[][] dp){
+        if(day==0){
 
+        }
+    }
 }
