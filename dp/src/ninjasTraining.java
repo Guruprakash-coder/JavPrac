0ccf9a1e2620887f83c7a4199eda69eeda3f7a9b
Guruprakash-coder <guruprakash6999@gmail.com>
2026-09-17 15:51:25 +0530

    update

--- dp/src/ninjasTraining.java
+++ dp/src/ninjasTraining.java
@@ -2,6 +2,8 @@ import java.util.Arrays;
 
 public class ninjasTraining {
     int ninjaTraining(int n,int[][] points){
+        StringBuilder sb=new StringBuilder();
+        
         int dp[][]=new int[n][4];
         for (int[] row : dp) {
             Arrays.fill(row, -1);
@@ -30,11 +32,25 @@ public class ninjasTraining {
         return dp[day][last]=maxi;
     }
     int ninjaTrainingTabulation(int n,int[][] points){
-        int[][] dp=[n][4];
+        int[][] dp=new int[n][4];
         dp[0][0]=Math.max(points[0][1],points[0][2]);
         dp[0][1]=Math.max(points[0][0],points[0][2]);
         dp[0][2]=Math.max(points[0][1],points[0][0]);
-        dp[0][0]=Math.max(points[0][3],Math.max(points[0][1],points[0][2]));
+        dp[0][3]=Math.max(points[0][0],Math.max(points[0][1],points[0][2]));
+        for(int day=1;day<n;day++){
+            for(int last=0;last<4;last++){
+                dp[day][last]=0;
+                int maxi=0;
+                for(int task=0;task<3;task++){
+                    if(task!=last){
+                        int point=points[day][task]+dp[day-1][task];
+                        maxi=Math.max(maxi,point);
+                    }
 
+                }
+                dp[day][last]=maxi;
+            }
+        }
+        return dp[n-1][3];
     }
 }
