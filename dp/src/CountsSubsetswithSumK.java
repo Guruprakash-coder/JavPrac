15a7a05f23aec0522fabba4a2642d61809b590b9
Guruprakash-coder <guruprakash6999@gmail.com>
2026-09-19 14:56:44 +0530

    update

--- dp/src/CountsSubsetswithSumK.java
+++ dp/src/CountsSubsetswithSumK.java
@@ -1,5 +1,24 @@
-public class CountsSubsetswithSumK {
-    public int count(){
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
+public class CountsSubsetswithSumK {
+    public int count(int[] arr,int k){
+        int[][] dp=new int[arr.length][k+1];
+        for(int[] d:dp){
+            Arrays.fill(d,-1);
+        }
+        return f(arr,arr.length-1,k,dp);
+    }
+    private int f(int[] arr,int ind,int sum,int[][] dp){
+        if(ind==0){
+            if(sum==arr[0]) return 1;
+            return 0;
+        }
+        if(sum==0) return 1;
+        if(dp[ind][sum]!=-1) return dp[ind][sum];
+        int notake=f(arr,ind-1,sum,dp);
+        int take=0;
+        if(sum>=arr[ind-1]) take=f(arr,ind-1,sum-arr[ind-1],dp);
+        return dp[ind][sum]=notake+take;
     }
 }
